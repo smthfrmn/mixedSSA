fbf1daba4fc62246801423a817bca17a8e458075
smthfrmn <smithifreeman@gmail.com>
2023-07-26 16:27:04 -0500

    update workflow

--- R/scratch.R
+++ R/scratch.R
@@ -1,77 +1,77 @@
-library(here)
-
-model <- readRDS(here("R/Snapper.ssf_sum_6min_all.rds"))
-
-model_data <- model$frame
-
-populate_reef <- function(model_data) {
-  reef <- apply(model_data, 1, function(row) {
-    if (row[["reefS2"]] == "1") {
-      return("S2")
-    } else if (row[["reefS3"]] == "1") {
-      return("S3")
-    } else if (row[["reefS4"]] == "1") {
-      return("S4")
-    } else {
-      return("S1")
-    }
-  })
-}
-
-new_model_data <- model_data %>%
-  mutate(
-    reef = as.factor(populate_reef(model_data))
-  )
-
-model$frame <- new_model_data
-
-
-# steps
-updated_steps_dist <- update_dist(
-  model = model,
-  dist_name = "gamma",
-  interaction_var_name = "reef",
-  beta_sl = "sl_",
-  beta_log_sl = "log_sl_"
-)
-
-plot_movement_distributions(updated_steps_dist)
-
-
-# tas
-updated_tas_dist <- update_distributions_by_categorical_var(
-  model = model,
-  dist_name = "vonmises",
-  interaction_var_name = "reef",
-  coef_names = c("cos(ta_)")
-)
-
-plot_movement_distributions(updated_tas_dist)
-
-model <- get_sample_models()[["gamma"]]
-
-updated_params <- update_dist(model,
-  dist_name = "gamma",
-  interaction_var_name = "sex",
-  beta_sl = "sl_",
-  beta_log_sl = "log_sl_"
-)
-
-model <- get_sample_models(interaction_var_name = "elevation")[["vonmises"]]
-
-updated_params <- update_dist(model,
-            dist_name = "vonmises",
-            interaction_var_name = "elevation",
-            beta_cos_ta = "cos_ta_"
-)
-
-
-model <- get_sample_mixed_models(interaction_var_name = "elevation")[["vonmises"]]
-
-updated_params <- update_dist(model,
-                              dist_name = "vonmises",
-                              interaction_var_name = "elevation",
-                              random_effects_var_name = "id",
-                              beta_cos_ta = "cos_ta_")
-
-
+# library(here)
+#
+# model <- readRDS(here("R/Snapper.ssf_sum_6min_all.rds"))
+#
+# model_data <- model$frame
+#
+# populate_reef <- function(model_data) {
+#   reef <- apply(model_data, 1, function(row) {
+#     if (row[["reefS2"]] == "1") {
+#       return("S2")
+#     } else if (row[["reefS3"]] == "1") {
+#       return("S3")
+#     } else if (row[["reefS4"]] == "1") {
+#       return("S4")
+#     } else {
+#       return("S1")
+#     }
+#   })
+# }
+#
+# new_model_data <- model_data %>%
+#   mutate(
+#     reef = as.factor(populate_reef(model_data))
+#   )
+#
+# model$frame <- new_model_data
+#
+#
+# # steps
+# updated_steps_dist <- update_dist(
+#   model = model,
+#   dist_name = "gamma",
+#   interaction_var_name = "reef",
+#   beta_sl = "sl_",
+#   beta_log_sl = "log_sl_"
+# )
+#
+# plot_movement_distributions(updated_steps_dist)
+#
+#
+# # tas
+# updated_tas_dist <- update_distributions_by_categorical_var(
+#   model = model,
+#   dist_name = "vonmises",
+#   interaction_var_name = "reef",
+#   coef_names = c("cos(ta_)")
+# )
+#
+# plot_movement_distributions(updated_tas_dist)
+#
+# model <- get_sample_models()[["gamma"]]
+#
+# updated_params <- update_dist(model,
+#   dist_name = "gamma",
+#   interaction_var_name = "sex",
+#   beta_sl = "sl_",
+#   beta_log_sl = "log_sl_"
+# )
+#
+# model <- get_sample_models(interaction_var_name = "elevation")[["vonmises"]]
+#
+# updated_params <- update_dist(model,
+#             dist_name = "vonmises",
+#             interaction_var_name = "elevation",
+#             beta_cos_ta = "cos_ta_"
+# )
+#
+#
+# model <- get_sample_mixed_models(interaction_var_name = "elevation")[["vonmises"]]
+#
+# updated_params <- update_dist(model,
+#                               dist_name = "vonmises",
+#                               interaction_var_name = "elevation",
+#                               random_effects_var_name = "id",
+#                               beta_cos_ta = "cos_ta_")
+#
+#
