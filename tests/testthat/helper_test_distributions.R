222bdc05966c38af7de96a478424d9c855b65d88
smthfrmn <smithifreeman@gmail.com>
2025-05-12 14:57:21 -0500

    messed up rda

--- tests/testthat/helper_test_distributions.R
+++ tests/testthat/helper_test_distributions.R
@@ -3,6 +3,7 @@ library(amt)
 library(here)
 library(glmmTMB)
 library(stringr)
+library(dplyr)
 
 REFERENCE_CATEGORY <- "M"
 SEXES <- c(REFERENCE_CATEGORY, "F")
@@ -24,61 +25,72 @@ get_data_path_root <- function() {
 }
 
 get_sample_fisher_data <- function(custom_coefs = FALSE) {
-  # data("amt_fisher")
-  #
-  # track <- amt_fisher %>%
-  #   make_track(x_, y_, t_ = t_, id = id, sex = sex, name = name) %>%
-  #   nest(data = !any_of(c("id", "sex", "name"))) %>%
-  #   mutate(
-  #     steps = map(data, function(x) {
-  #       steps_final <- x %>%
-  #         track_resample(rate = minutes(30), tolerance = minutes(5)) %>%
-  #         steps_by_burst()
-  #       return(steps_final)
-  #     })
-  #   ) %>%
-  #   unnest(cols = steps) %>%
-  #   select(-data)
-  #
-  #
-  # m1 <- track %>%
-  #   filter(id == "M1") %>%
-  #   amt::random_steps()
-  #
-  # m4 <- track %>%
-  #   filter(id == "M4") %>%
-  #   amt::random_steps()
-  #
-  # f2 <- track %>%
-  #   filter(id == "F2") %>%
-  #   amt::random_steps()
-  #
-  # f1 <- track %>%
-  #   filter(id == "F1") %>%
-  #   amt::random_steps()
-  #
-  # data <- rbind(
-  #     m1, m4, f2, f1
-  #   ) %>%
-  #   within(sex <- relevel(as.factor(sex), ref = REFERENCE_CATEGORY)) %>%
-  #   mutate(
-  #     sl_sq_ = sl_ * sl_,
-  #     log_sl_ = log(sl_),
-  #     log_sl_sq_ = log_sl_ * log_sl_,
-  #     cos_ta_ = cos(ta_),
-  #     sex_three_factors = factor(ifelse(id == "F2", as.character("UNK"), as.character(sex)))
-  #   ) %>%
-  #   extract_covariates(terra::unwrap(amt_fisher_covar$elevation)) |>
-  #   mutate(
-  #     elevation_fact = case_when(
-  #       elevation < 45 + 35 ~ "low",
-  #       elevation < 45 + 70 ~ "medium",
-  #       elevation < 45 + 100 ~ "high"
-  #     ) |> as.factor()
-  #   )
+  data("amt_fisher")
+
+  track <- amt_fisher %>%
+    make_track(x_, y_, t_ = t_, id = id, sex = sex, name = name) %>%
+    nest(data = !any_of(c("id", "sex", "name"))) %>%
+    mutate(
+      steps = map(data, function(x) {
+        steps_final <- x %>%
+          track_resample(rate = minutes(30), tolerance = minutes(5)) %>%
+          steps_by_burst()
+        return(steps_final)
+      })
+    ) %>%
+    unnest(cols = steps) %>%
+    select(-data)
+
+
+  m1 <- track %>%
+    filter(id == "M1") %>%
+    amt::random_steps()
+
+  m4 <- track %>%
+    filter(id == "M4") %>%
+    amt::random_steps()
+
+  f2 <- track %>%
+    filter(id == "F2") %>%
+    amt::random_steps()
+
+  f1 <- track %>%
+    filter(id == "F1") %>%
+    amt::random_steps()
+
+  data <- rbind(
+      m1, m4, f2, f1
+    ) %>%
+    within(sex <- relevel(as.factor(sex), ref = REFERENCE_CATEGORY)) %>%
+    mutate(
+      sl_sq_ = sl_ * sl_,
+      log_sl_ = log(sl_),
+      log_sl_sq_ = log_sl_ * log_sl_,
+      cos_ta_ = cos(ta_),
+      sex_three_factors = factor(ifelse(id == "F2", as.character("UNK"), as.character(sex)))
+    ) %>%
+    extract_covariates(terra::unwrap(amt_fisher_covar$elevation)) |>
+    extract_covariates(terra::unwrap(amt_fisher_covar$elevation), where = "both") |>
+    mutate(
+      elevation_fact = case_when(
+        elevation < 45 + 35 ~ "low",
+        elevation < 45 + 70 ~ "medium",
+        elevation < 45 + 100 ~ "high"
+      ) |> as.factor(),
+      elevation_fact_start = case_when(
+        elevation_start < 45 + 35 ~ "low",
+        elevation_start < 45 + 70 ~ "medium",
+        elevation_start < 45 + 100 ~ "high"
+      ) |> as.factor(),
+      elevation_fact_end = case_when(
+        elevation_start < 45 + 35 ~ "low",
+        elevation_start < 45 + 70 ~ "medium",
+        elevation_start < 45 + 100 ~ "high"
+      ) |> as.factor()
+    )
 
   file_path <- str_interp("${get_data_path_root()}/fisher_data.rds")
-  # saveRDS(data, file_path)
+  saveRDS(data, file_path)
   data <- readRDS(file_path)
   if (custom_coefs) {
     # If the user isn't using the amt nomenclature
